250ad54ad422ec5de7bc08cd3802a10dc7aa0cf7
chenyaxun <chenyaxun@huawei.com>
2021-06-25 17:43:26 +0800

    Add init testcase

---
+++ src/test/java/ohos/oat/utils/OatCfgUtilTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ohos.oat.utils;
+
+import ohos.oat.config.OatConfig;
+
+import org.junit.Assert;
+
+/**
+ * @author chenyaxun
+ * @since 1.0
+ */
+public class OatCfgUtilTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        System.out.println("setUp");
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+        System.out.println("teardown");
+    }
+
+    @org.junit.Test
+    public void getShortPath() {
+        final OatConfig oatConfig = new OatConfig();
+        oatConfig.setBasedir("C:/_chen/TestFiles/");
+        final String filePath = "C:/_chen/TestFiles/ApacheLicense.txt";
+        final String result = OatCfgUtil.getShortPath(oatConfig, filePath);
+        Assert.assertEquals(result, "ApacheLicense.txt");
+    }
+
+    @org.junit.Test
+    public void formatPath() {
+    }
+
+    @org.junit.Test
+    public void testGetShortPath() {
+    }
+
+    @org.junit.Test
+    public void initOatConfig() {
+    }
+
+    @org.junit.Test
+    public void getSplitStrings() {
+    }
+
+    @org.junit.Test
+    public void testGetSplitStrings() {
+    }
+}
