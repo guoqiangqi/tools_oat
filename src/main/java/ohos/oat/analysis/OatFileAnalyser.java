14754ccdada57f51cbd3f1f72151b3f0c85ae4ec
chenyaxun <chenyaxun@huawei.com>
2022-09-06 21:37:27 +0800

    Decouple the processing flow and extract interfaces
    Signed-off-by: chenyaxun <chenyaxun@huawei.com>

--- src/main/java/ohos/oat/analysis/OatFileAnalyser.java
+++ src/main/java/ohos/oat/analysis/OatFileAnalyser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -141,7 +141,7 @@ public class OatFileAnalyser {
         "doxyfile"
 
     };
- 
+
     private final List<String> oriHeaderTextList = new ArrayList<>();
 
     private final StringBuffer oriHeaderText = new StringBuffer();
