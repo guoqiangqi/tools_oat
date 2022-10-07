c8c1d67b75db85d911cf2f2c862295f711d92ab5
chenyaxun <chenyaxun@huawei.com>
2022-10-07 23:46:31 +0800

    Add report info data structure
    Signed-off-by: chenyaxun <chenyaxun@huawei.com>

---
+++ src/main/java/ohos/oat/reporter/model/OatReportBasicInfo.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ohos.oat.reporter.model;
+
+/**
+ * @author chenyaxun
+ * @since 2.0
+ */
+public class OatReportBasicInfo {
+    private final String reportTime = "";
+
+    private final String reportUser = "";
+
+    private final String reportTool = "OAT";
+
+    private final String reportToolVersion = "";
+
+    private final String reportNotes = "Commemorate D";
+
+    private final String ossProjectPath = "";
+
+    private final String ossProjectBranch = "";
+
+    private final String ossProjectTag = "";
+
+    private final long ossProjectFileCount = 0;
+
+    private final long ossProjectNormalFileCount = 0;
+
+    private final long ossProjectFilteredFileCount = 0;
+
+    private final long ossProjectFilteredByHeaderFileCount = 0;
+
+    private final long ossProjectLicenseTypeCount = 0;
+
+    private final long ossProjectNormalLicenseTypeCount = 0;
+
+    private final long ossProjectAbnormalLicenseTypeCount = 0;
+
+    private final long ossProjectNoLicenseFileCount = 0;
+
+    private final long ossProjectCopyrightCount = 0;
+
+    private final long ossProjectNormalCopyrightCount = 0;
+
+    private final long ossProjectAbnormalCopyrightCount = 0;
+
+    private final long ossProjectNoCopyrightFileCount = 0;
+
+}
